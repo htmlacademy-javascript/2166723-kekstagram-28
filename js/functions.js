4f19d19a706affa3172758e3ee0abe0ee5f4ec2f
Artem Samokhin <artem.samokhin.2001@mail.ru>
2023-02-24 12:00:06 +0300

    Создаёт 4 функции

--- js/functions.js
+++ js/functions.js
@@ -7,4 +7,43 @@ function checkLengthString (string, lengthString) {
   }
 }
 
-checkLengthString('проверяемая строка', 10);
+checkLengthString();
+
+function checkPalindrome (string) {
+  const lowerString = string.toLowerCase().replaceAll(' ', '');
+  let reverseString = '';
+  for (let i = lowerString.length - 1; i >= 0; i--) {
+    reverseString += lowerString.at(i);
+  }
+  return lowerString === reverseString;
+}
+
+checkPalindrome();
+
+function searchNumber (string) {
+  if (typeof string === 'number') {
+    return string;
+  }
+
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+  return parseInt (result, 10);
+}
+
+searchNumber();
+
+function myPadStart (string, minLength, pad) {
+  const actualPad = minLength - string.length;
+
+  if (actualPad <= 0) {
+    return string;
+  }
+
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
+}
+
+myPadStart();
