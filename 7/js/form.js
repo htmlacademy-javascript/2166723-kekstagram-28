a525f530d6b88d803db41bce8adcb47269932e80
Keks <keks@htmlacademy.ru>
2023-04-06 09:30:10 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/form.js
+++ 7/js/form.js
@@ -1,6 +1,7 @@
 const TAG_ERROR_TEXT = 'Неправильно заполнены хэштеги';
-const VALID_SIMBOLS = /^#{a-zа-яё0-9}{1,19}$/i;
+const VALID_SIMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
 const MAX_HASHTAG_COUNT = 5;
+
 const form = document.querySelector('.img-upload__form');
 const overlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
@@ -9,7 +10,7 @@ const fileField = document.querySelector('#upload-file');
 const hashtagField = document.querySelector('.text__hashtags');
 const commentField = document.querySelector('.text__description');
 
-const pristine = new Pristine (form, {
+const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass: 'img-upload__field-wrapper__error',
@@ -33,19 +34,19 @@ const isTextFieldFocused = () =>
   document.activeElement === hashtagField ||
   document.activeElement === commentField;
 
-function onDocumentKeydown (evt) {
+function onDocumentKeydown(evt) {
   if (evt.key === 'Escape' && !isTextFieldFocused()) {
     evt.preventDefault();
     hideModal();
   }
 }
 
-const onFileInputChange = () => {
-  showModal();
-};
-
 const onCancelButtonClick = () => {
   hideModal();
+};
+
+const onFileInputChange = () => {
+  showModal();
 };
 
 const isValidTag = (tag) => VALID_SIMBOLS.test(tag);
@@ -54,21 +55,21 @@ const hasValidCount = (tags) => tags.length <= MAX_HASHTAG_COUNT;
 
 const hasUniqueTags = (tags) => {
   const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
-  return lowerCaseTags.length === new Set (lowerCaseTags).size;
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
 };
 
 const validateTags = (value) => {
   const tags = value
-    .thim()
+    .trim()
     .split(' ')
-    .filter((tag) => tag.thim().length);
+    .filter((tag) => tag.trim().length);
   return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
 };
 
 pristine.addValidator(
   hashtagField,
   validateTags,
-  TAG_ERROR_TEXT,
+  TAG_ERROR_TEXT
 );
 
 const onFormSubmit = (evt) => {
